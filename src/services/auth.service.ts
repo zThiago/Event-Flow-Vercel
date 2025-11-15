f5884a6bdf6699fa193b0ffa6bf6d725a1ab5ae0
Thiago Cunha Ferreira <50816917+zThiago@users.noreply.github.com>
2025-11-15 14:19:25 -0300

    Fix import path for UsersService in auth.service.ts

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { UsersService } from "src/services/users.service";
+import { UsersService } from "../services/users.service";
 import * as bcrypt from "bcryptjs";
 
 @Injectable()
