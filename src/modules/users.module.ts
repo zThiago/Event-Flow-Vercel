feb9deb09b97c3730535bb2f92dafceca31a402d
Thiago Cunha Ferreira <50816917+zThiago@users.noreply.github.com>
2025-11-15 14:22:00 -0300

    Fix import path for UsersController

--- src/modules/users.module.ts
+++ src/modules/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
-import { UsersController } from 'src/controllers/users.controller';
+import { UsersController } from '../controllers/users.controller';
 
 @Module({
   controllers: [UsersController],
